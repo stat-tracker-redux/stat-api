7ebb5906fd14436551dfda1319e38616e134f218
Jonathan Frederick <jrfreder@gmail.com>
2016-03-17 17:56:15 -0400

    moved unit and functional tests to a tests directory in app

--- functional_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-class SmokeTest(unittest.TestCase):
-    def test_func_false_is_not_true(self):
-        self.assertTrue(False)
-
-if __name__ == '__main__':
-    unittest.main()
