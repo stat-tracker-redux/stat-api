ae4223a12421dceb671ad4260f32322d42a1f675
Jonathan Frederick <jrfreder@gmail.com>
2016-03-17 20:54:18 -0400

    added docstring and data to register test

--- stats/api/tests/test_api_functionality.py
+++ stats/api/tests/test_api_functionality.py
@@ -5,6 +5,13 @@ from requests import get, post
 
 class UserExperienceTestCase(LiveServerTestCase):
     def test_can_create_a_new_user(self):
-        register_resp = get(self.live_server_url + '/register/')
+        """
+        Test that the /register/ endpoint can succesfully create a new user
+        """
+        # Todd registers with his email and password
+        register_resp = get(self.live_server_url + '/register/',
+                            data={"username": "superlunk360",
+                                  "email": "todd.mcbuffy@gmail.com",
+                                  "password": "supersecret"})
         self.assertEquals(register_resp.status_code, 200)
         self.assertEquals(register_resp.text, '')
