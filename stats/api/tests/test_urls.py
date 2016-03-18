6fc09207213335308952dc7fdb9731b170a3091f
Jonathan Frederick <jrfreder@gmail.com>
2016-03-17 21:23:08 -0400

    wrote test for register endpoint

--- stats/api/tests/test_urls.py
+++ stats/api/tests/test_urls.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
 
+from django.core.urlresolvers import resolve
+
+from api.views import create_user
+
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+class UserURLsTest(TestCase):
+    def test_register_url_resolves_to_create_user_view(self):
+        """
+        Tests that /register/ endpoint resolves to a view titled create_user
+        """
+        found_create_user = resolve('register')
+        self.assertEqual(found_create_user.func, create_user)
