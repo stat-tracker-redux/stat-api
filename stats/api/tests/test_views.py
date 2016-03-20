18fdb34b8ff24207fb4472fedacc16dddafa519b
Jonathan Frederick <jrfreder@gmail.com>
2016-03-20 18:03:52 -0400

    testing that only post works for register endpoint

--- stats/api/tests/test_views.py
+++ stats/api/tests/test_views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 from api.views import user_create
 
+
 class UserViewsTest(TestCase):
     def test_register_url_returns_empty_string(self):
         """
@@ -10,3 +11,15 @@ class UserViewsTest(TestCase):
         request = HttpRequest()
         response = user_create(request)
         self.assertEqual(response.content, b'')
+
+    def test_register_url_requires_post(self):
+        """
+        Tests that POST requests get through, but others do not
+        """
+        post_request = HttpRequest()
+        post_request.method = 'POST'
+        get_request = HttpRequest()
+        post_response = user_create(post_request)
+        get_response = user_create(get_request)
+        self.assertEqual(post_response.status_code, 200)
+        self.assertNotEqual(get_response.status_code, 200)
