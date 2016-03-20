96a6d138f7e1c39d3c1b46c341e3e0e206179ed2
Jonathan Frederick <jrfreder@gmail.com>
2016-03-19 21:30:41 -0400

    refactored to give post request and to check for the new user

--- stats/api/tests/test_api_functionality.py
+++ stats/api/tests/test_api_functionality.py
@@ -2,6 +2,8 @@ from django.test import TestCase, LiveServerTestCase
 
 from requests import get, post
 
+from django.contrib.auth.models import User
+
 
 class UserExperienceTestCase(LiveServerTestCase):
     def test_can_create_a_new_user(self):
@@ -9,9 +11,15 @@ class UserExperienceTestCase(LiveServerTestCase):
         Test that the /register/ endpoint can succesfully create a new user
         """
         # Todd registers with his email and password
-        register_resp = get(self.live_server_url + '/register/',
-                            data={"username": "superlunk360",
-                                  "email": "todd.mcbuffy@gmail.com",
-                                  "password": "supersecret"})
+        register_resp = post(self.live_server_url + '/register/',
+                             data={"username": "superlunk360",
+                                   "email": "todd.mcbuffy@gmail.com",
+                                   "password": "supersecret"})
         self.assertEquals(register_resp.status_code, 200)
         self.assertEquals(register_resp.text, '')
+
+        self.assertEqual(User.objects.get(username='superlunk360').email(),
+                         'todd.mcbuffy@gmail.com')
+        self.assertEqual(User.objects.get(
+                         email='todd.mcbuff@gmail.com').username(),
+                         'superlunk360')
