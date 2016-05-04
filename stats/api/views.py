fa995acb6bdbbae182ab2c3d5812d0af20e1159f
Jonathan Frederick <jrfreder@gmail.com>
2016-05-03 20:29:55 -0400

    added csrf_exempt to pass 200 check

--- stats/api/views.py
+++ stats/api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 # Create your views here.
 
 
+@csrf_exempt
 def user_create(request):
     if request.method == 'POST':
         return HttpResponse('')
