f70055212d0f0fed2c4a264cba20f1542b39e3c6
Jonathan Frederick <jrfreder@gmail.com>
2016-05-03 20:15:15 -0400

    added csrf exemption, attempting sliming user creation

--- stats/api/views.py
+++ stats/api/views.py
@@ -1,12 +1,24 @@
+import json
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
+@csrf_exempt
 def user_create(request):
     if request.method == 'POST':
+        user = User.objects.create_user('superlunk360', 'todd.mcbuffy@gmail.com', 'supersecret')
+
+        user_info = json.loads(request.body.decode('utf-8'))
+        user = User.objects.create_user(user_info['username'],
+                                        user_info['email'],
+                                        user_info['password'])
+
+        user.save()
+
         return HttpResponse('')
     else:
         return HttpResponse('', status=500)
