83224c16557825ea8b97c7b4307359b449d963cf
Jonathan Frederick <jrfreder@gmail.com>
2016-05-05 15:20:33 -0400

    split test for post v get to register

--- stats/api/tests/test_views.py
+++ stats/api/tests/test_views.py
@@ -1,7 +1,6 @@
 from django.http import HttpRequest
 from django.test import TestCase
 from api.views import user_create
-from django.contrib.auth.models import User
 
 
 class UserViewsTest(TestCase):
@@ -13,29 +12,19 @@ class UserViewsTest(TestCase):
         response = user_create(request)
         self.assertEqual(response.content, b'')
 
-    def test_register_url_requires_post(self):
+    def test_register_url_post_returns_200(self):
         """
-        Tests that POST requests get through, but others do not
+        Tests that POST requests get through to register endpoint
         """
         post_request = HttpRequest()
         post_request.method = 'POST'
-        get_request = HttpRequest()
         post_response = user_create(post_request)
-        get_response = user_create(get_request)
         self.assertEqual(post_response.status_code, 200)
-        self.assertNotEqual(get_response.status_code, 200)
 
-    def test_register_url_creates_user(self):
+    def test_register_url_get_returns_error(self):
         """
-        Tests that the view creates a user in the database
+        Tests that GET requests to register endpoint return errors
         """
-        post_request = HttpRequest()
-        post_request.method = 'POST'
-        post_request.POST['username'] = 'brother.bear'
-        post_request.POST['password'] = 'password'
-        post_request.POST['email'] = 'brother@berenstain.com'
-        response = user_create(post_request)
-        self.assertEqual(User.objects.get(username='brother.bear').email,
-                         'brother@berenstain.com')
-        self.assertEqual(User.objects.get(email='brother@berenstain.com').username,
-                         'brother.bear')
+        get_request = HttpRequest()
+        get_response = user_create(get_request)
+        self.assertNotEqual(get_response.status_code, 200)
