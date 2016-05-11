8875a3dad1be25a7f3b90e509a9332d185df73d7
Jonathan Frederick <jrfreder@gmail.com>
2016-05-11 18:45:42 -0400

    scaling back to checking useless post

--- stats/api/tests/test_views.py
+++ stats/api/tests/test_views.py
@@ -1,14 +1,21 @@
 from django.http import HttpRequest
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
 from api.views import user_create
 
 
 class UserViewsTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='test', email='test@email.com', password='password'
+        )
+
     def test_register_url_returns_empty_string(self):
         """
         Tests that a request to the register url returns nothing
         """
-        request = HttpRequest()
+        request = self.factory.get('/register/')
         response = user_create(request)
         self.assertEqual(response.content, b'')
 
@@ -16,8 +23,9 @@ class UserViewsTest(TestCase):
         """
         Tests that POST requests get through to register endpoint
         """
-        post_request = HttpRequest()
-        post_request.method = 'POST'
+        # json_string = '{"username": "user", "email": "user@email.com", "password": "supersecret"}'
+        json_string = '{"things": "stuff"}'
+        post_request = self.factory.post('/register/', content_type='application/json', data=json_string)
         post_response = user_create(post_request)
         self.assertEqual(post_response.status_code, 200)
 
@@ -25,6 +33,6 @@ class UserViewsTest(TestCase):
         """
         Tests that GET requests to register endpoint return errors
         """
-        get_request = HttpRequest()
+        get_request = self.factory.get('/register/')
         get_response = user_create(get_request)
         self.assertNotEqual(get_response.status_code, 200)
